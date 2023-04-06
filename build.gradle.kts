7ef815e9f1ce4649da4be0c9f73d87bd67d0f9e2
Johannes Oehm <johannes.oehm@uni-muenster.de>
2023-04-06 12:55:59 +0200

    update kotlin version and jvm target

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.8.20"
     application
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
@@ -50,7 +50,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 application {
